e4f16e268b4aef0bbad6da56a4b6163a11b3861d
LifetimeMistake <szymek2532yt@gmail.com>
2019-11-28 09:12:46 +0100

    Foundation for the packet receiver

--- coolRAT-libs/Connections/DualTcpConnection.cs
+++ coolRAT-libs/Connections/DualTcpConnection.cs
@@ -1,12 +1,37 @@
-﻿using System;
+﻿using coolRAT.Libs.Packets;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace coolRAT.Libs.Connections
 {
+    public class PacketReceivedEventArgs : EventArgs
+    {
+        Packet Packet;
+
+        public PacketReceivedEventArgs(Packet packet)
+        {
+            Packet = packet;
+        }
+    }
     public class DualTcpConnection
     {
         public TcpConnection IncomingConnection;
         public TcpConnection OutgoingConnection;
+
+        public delegate void PacketReceivedHandler(object sender, PacketReceivedEventArgs e);
+        public Dictionary<string, PacketReceivedHandler> PacketReceiverSubscriptions;
+
+        public DualTcpConnection(TcpConnection incomingConnection, TcpConnection outgoingConnection, Dictionary<string, PacketReceivedHandler> packetReceiverSubscriptions)
+        {
+            IncomingConnection = incomingConnection;
+            OutgoingConnection = outgoingConnection;
+            PacketReceiverSubscriptions = packetReceiverSubscriptions;
+        }
+
+        public DualTcpConnection()
+        {
+            PacketReceiverSubscriptions = new Dictionary<string, PacketReceivedHandler>();
+        }
     }
 }
