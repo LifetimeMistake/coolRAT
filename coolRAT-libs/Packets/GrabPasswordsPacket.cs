774e9a6d88d1cfbe2fc7fc1c2b6cb4dbbe2b3779
LifetimeMistake <szymek2532yt@gmail.com>
2019-12-04 20:03:26 +0100

    Grab Password changes

---
+++ coolRAT-libs/Packets/GrabPasswordsPacket.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace coolRAT.Libs.Packets
+{
+    public class GrabPasswordsPacket : Packet
+    {
+        public GrabPasswordsPacket(Guid uniqueClientId) : base(uniqueClientId)
+        {
+            Type = "GrabPasswordsPacket";
+        }
+
+        public new string Serialize()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+
+        public new static GrabPasswordsPacket Deserialize(string packet_string)
+        {
+            return JsonConvert.DeserializeObject<GrabPasswordsPacket>(packet_string);
+        }
+    }
+}
